b716e0a22053dd9244815a8dfc50894f62311c0e
Nathan <a1@1s-MacBook-Pro.local>
2020-02-19 01:04:48 +0200

    eegeg

--- deploy.py
+++ deploy.py
@@ -18,7 +18,8 @@ def index():
     head = str(request.headers)
     resp = flask.make_response(parser(head))
     resp.headers['Access-Control-Allow-Origin'] = '*'
-    return requests.get("https://stormy-earth-91493.herokuapp.com/",headers={"data":head}).text
+    return head
+    #return requests.get("https://stormy-earth-91493.herokuapp.com/",headers={"data":head}).text
     
 
 def parser(string):
